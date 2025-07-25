c07163c5d1da9c8e6f4d6ea3c2f84bc7ece5f5f5
tokon2000 <99998548+tokon2000@users.noreply.github.com>
2025-07-25 20:09:39 +0200

    Update start-mysqld.sh

--- supporting_files/start-mysqld.sh
+++ supporting_files/start-mysqld.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
+
+# Alte PID-Datei entfernen falls vorhanden
+if [ -f /var/run/mysqld/mysqld.pid ]; then
+    rm -f /var/run/mysqld/mysqld.pid
+fi
+
+# Alte Socket-Dateien entfernen falls vorhanden
+if [ -S /var/run/mysqld/mysqld.sock ]; then
+    rm -f /var/run/mysqld/mysqld.sock
+fi
+
+if [ -S /var/run/mysqld/mysqlx.sock ]; then
+    rm -f /var/run/mysqld/mysqlx.sock
+fi
+
+# Kurze Pause um sicherzustellen, dass andere Prozesse nicht kollidieren
+sleep 2
+
+# MySQL starten
 exec /usr/local/bin/pidproxy /var/run/mysqld/mysqld.pid /usr/bin/mysqld_safe
